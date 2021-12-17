7c804da04e2677d5e167b9b1d353fee335a6b677
EscapeFromJava <reske.artur@gmail.com>
2021-12-17 23:32:37 +0300

    Добавьте файлы проекта.

---
+++ Game_Arkanoid/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Game_Arkanoid
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.unit = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.labelScore = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.unit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // unit
+            // 
+            this.unit.BackColor = System.Drawing.Color.Transparent;
+            this.unit.BackgroundImage = global::Game_Arkanoid.Properties.Resources.unit;
+            this.unit.Location = new System.Drawing.Point(375, 350);
+            this.unit.Name = "unit";
+            this.unit.Size = new System.Drawing.Size(50, 50);
+            this.unit.TabIndex = 0;
+            this.unit.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Location = new System.Drawing.Point(12, 9);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(48, 15);
+            this.labelScore.TabIndex = 2;
+            this.labelScore.Text = "Score: 0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkGreen;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 420);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 30);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.unit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.unit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox unit;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Game_Arkanoid/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_Arkanoid
+{
+    public partial class Form1 : Form
+    {
+        int enemySpeed = 1;
+        int enemyVolume = 6;
+        bool enemyRigth = false;
+        bool enemyLeft = false;
+        bool goLeft = false;
+        bool goRigth = false;
+        int enemyKill = 0;
+        int speedUnit = 25;
+
+        public Form1()
+        {
+            InitializeComponent();
+            spawnEnemy(enemyVolume);
+            enemyRigth = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+                unit.Left -= speedUnit;
+            if (e.KeyCode == Keys.Right)
+                unit.Left += speedUnit;
+            if (e.KeyCode == Keys.Space)
+                shoot();
+        }
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            labelScore.Text = "Score: " + enemyKill;
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag == "shoot")
+                {
+                    x.Top -= 15;
+                    foreach (Control y in this.Controls)
+                    {
+                        if (y is PictureBox && y.Tag == "enemy")
+                        {
+                            if (x.Bounds.IntersectsWith(y.Bounds))
+                            {
+                                enemyKill++;
+                                x.Dispose();
+                                y.Dispose();
+                                if (enemyKill % 6 == 0)
+                                {
+                                    enemySpeed++;
+                                    spawnEnemy(enemyVolume);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag == "enemy")
+                {
+                    moveEnemy(x);
+                }
+            }
+        }
+
+        private void shoot()
+        {
+            PictureBox shoot = new PictureBox();
+            shoot.Tag = "shoot";
+            shoot.BackgroundImage = Properties.Resources.rocket;
+            shoot.Left = unit.Left + 20;
+            shoot.Top = unit.Top;
+            shoot.Size = new Size(10, 10);
+            this.Controls.Add(shoot);
+            shoot.BringToFront();
+        }
+
+        private void moveEnemy(Control y)
+        {
+            if (enemyRigth)
+            {
+                y.Left += enemySpeed;
+                if (y.Left > 700)
+                {
+                    enemyRigth = false;
+                    enemyLeft = true;
+                }
+
+            }
+            if (enemyLeft)
+            {
+                y.Left -= enemySpeed;
+                if (y.Left < 100)
+                {
+                    enemyRigth = true;
+                    enemyLeft = false;
+                }
+
+            }
+        }
+        private void spawnEnemy(int x)
+        {
+            int temp = 75;
+            for (int i = 0; i < x; i++)
+            {
+                PictureBox enemy = new PictureBox();
+                enemy.Tag = "enemy";
+                enemy.BackgroundImage = Properties.Resources.enemy;
+                enemy.Left = i + temp;
+                enemy.Top = 50;
+                enemy.Size = new Size(50, 50);
+                this.Controls.Add(enemy);
+                enemy.BringToFront();
+                temp += 75;
+            }
+        }
+    }
+}
